86bbf234d384caceb5c5414f3df19f527793d6fa
GM <gladys_molina@hotmail.com>
2026-02-04 06:36:32 -0300

    corrigiendo ...

--- app.mjs
+++ app.mjs
@@ -24,12 +24,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
-
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method')); // leer como método DELETE viniendo de un POST
